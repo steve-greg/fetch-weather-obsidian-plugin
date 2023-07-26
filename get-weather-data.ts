789f7409bce089ddf0d69133440d47f3f862afc6
Stephen Gregory <stephen.gregory@amwins.com>
2023-07-25 17:16:23 -0700

    init

---
+++ get-weather-data.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosResponse } from "axios";
+import { Notice } from "obsidian";
+import { from, shareReplay, switchMap, tap, timer } from "rxjs";
+
+const delay = 0;
+const interval = 300000;
+
+export function getWeatherDataOnTimer(city?: string | null, format?: string) {
+	var url = `https://wttr.in/`;
+	if (city) {
+		url = `https://wttr.in/${city}`;
+	}
+	if (format) {
+		url += `?format="${format}"`;
+	}
+	return timer(delay, interval).pipe(
+		switchMap(() => from(axios.get(url))),
+		switchMap((response: AxiosResponse) => {
+			new Notice("Weather data updated");
+			if (response.status !== 200) {
+				new Notice("Error fetching weather data");
+				return [];
+			}
+			return [response.data];
+		}),
+		shareReplay(1)
+	);
+}
+
+export function getWeatherData(city: string) {
+	var url = `https://wttr.in/${city}?format=3`;
+	var promise = axios.get(url).then((response: AxiosResponse) => {
+		if (response.status !== 200) {
+			new Notice("Error fetching weather data");
+			return [];
+		}
+		return response.data;
+	});
+
+	return from(promise);
+}
